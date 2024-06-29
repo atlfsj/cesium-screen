3e88b87e11002251f268a260765c9630f03a86a7
atlfsj <3321713512@qq.com>
2024-06-29 19:07:47 +0800

    部署代码

---
+++ static/serverconfig.js
@@ -0,0 +1,220 @@
+window.global_url = {
+  API_URL: 'http://123.57.65.1:80',
+  MODEL_URL: './model/tileset.json',
+  // 监控配置
+  MONITOR_CONFIG: [
+    {
+      label: "设CODcr在线分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.15249275111007, 38.95527439635718, 0],
+      url: "/01.mp4",
+      origin: 1,
+      ins_date: '2023-11-08',
+      admin: '郭亚栋',
+      area: '科研区'
+    },
+    {
+      label: "TP在线分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.1234571564235, 38.97509332775813, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2023-11-06',
+      admin: '高慧慧',
+      area: '保育区'
+    },
+    {
+      label: "土壤参数监测仪",
+      type: "system",
+      icon: 13,
+      point: [117.14316602510617, 38.96137283884687, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '李昱泽',
+      area: '科研区'
+    },
+    {
+      label: "溶解氧监测仪",
+      type: "system",
+      icon: 13,
+      point: [117.1491663277498, 38.96165225272396, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2023-10-23',
+      admin: '陆靖铭',
+      area: '科研区'
+    },
+    {
+      label: "ORP分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.15810796772006, 38.94796703272158, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '李嘉峻',
+      area: '科研区'
+    },
+    {
+      label: "ORP分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.18710987165471, 38.92970137167191, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2023-07-23',
+      admin: '郭亚栋',
+      area: '科研区'
+    },
+    {
+      label: "监控2",
+      type: "monitor",
+      icon: 13,
+      point: [117.13339102034477, 38.969787702274516, 0],
+      url: "",
+      origin: 4
+    },
+    {
+      label: "监控3",
+      type: "monitor",
+      icon: 13,
+      point: [117.15860615332423, 38.94446373582399, 0],
+      url: "",
+      origin: 1
+    },
+    {
+      label: "监控4",
+      type: "monitor",
+      icon: 13,
+      point: [117.1226172383957, 38.97913841581083, 0],
+      url: "",
+      origin: 1
+    },
+    {
+      label: "监控5",
+      type: "monitor",
+      icon: 13,
+      point: [117.13076605045343, 38.97562969369748, 0],
+      url: "",
+      danger: true,
+      origin: 2
+    },
+    {
+      label: "1号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.16416189582296, 38.94222449707036, 0],
+      url: "",
+      origin: 3
+    },
+    {
+      label: "2号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.15847972025885, 38.93898562532035, 0],
+      url: "",
+      origin: 3
+    },
+    {
+      label: "3号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.17656637333333, 38.92647099980325, 0],
+      url: "",
+      origin: 3
+    },
+    {
+      label: "4号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.18045289655892, 38.920090531836635, 0],
+      url: "",
+      danger: true,
+      origin: 1
+    },
+    {
+      label: "5号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.16616425202886, 38.932036700046424, 0],
+      url: "",
+      origin: 2
+    },
+    {
+      label: "6号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.16616425202886, 38.942036700046424, 0],
+      url: "/01.mp4",
+      origin: 2
+
+    },
+    {
+      label: "水质监测仪",
+      type: "system",
+      admin: "李昱泽",
+      icon: 13,
+      ins_date: '2023-11-06',
+      point: [113.629331, 40.050210, 0],
+      url: "",
+      danger: true,
+      area: '科研区',
+      origin: 2
+
+
+    },
+    {
+      label: "水质监测仪",
+      type: "system",
+      admin: "高慧慧",
+      icon: 13,
+      ins_date: '2023-11-06',
+      point: [118.06722, 24.44224, 0],
+      url: "",
+      danger: false,
+      area: '科研区',
+      origin: 2
+
+
+    },
+    {
+      label: "水质监测仪",
+      type: "system",
+      admin: "陆靖铭",
+      icon: 13,
+      ins_date: '2023-09-06',
+      point: [117.143061, 38.969061, 0],
+      url: "",
+      danger: false,
+      origin: 2,
+      area: '科研区'
+    },
+
+
+
+
+    {
+      label: "7号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.187800, 38.924491, 0],
+      url: "",
+      origin: 2
+
+    },
+    {
+      label: "8号监测点",
+      type: "monitor",
+      icon: 13,
+      admin: "李嘉峻",
+      point: [117.190654, 38.929221, 0],
+      url: "/03.mp4",
+      origin: 2
+
+    }
+
+  ],
+};
